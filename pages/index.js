1acb86ccf185d222868dad7c979cf13a11278e7b
Djoudi21 <abdelkrim@gdpr.dev>
2022-12-23 20:18:28 +0100

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,34 @@
 const searchBtn = document.getElementById('search-btn')
 const searchInput = document.getElementById('search-input')
-const filterIngredient = document.getElementById('filter-ingredient')
-
-searchBtn.addEventListener('click', async () => {
-    const recipes = await getRecipes()
-    await displayData(recipes)
+const filterIngredientInput = document.getElementById('filter-ingredient')
+const recipesSection = document.getElementById('recipes')
+const filterListSection = document.getElementById("filter-elements");
+let filteredIngredientsToDisplay = []
+searchInput.addEventListener('focus', () => {
+    searchInput.value = ""
 })
 
-filterIngredient.addEventListener('keydown', async (event) => {
-    if (event.code === "Enter") {
-        await displayFilterList(event.target.value)
-        event.target.value = ""
-    }
+searchBtn.addEventListener('click', async () => {
+    recipesSection.innerHTML = ""
+    const recipes = await getRecipes()
+    await displayData(recipes, searchInput.value.trim())
+})
+
+filterIngredientInput.addEventListener('focus', async (event) => {
+    filterListSection.classList.add('display-grid')
+})
+
+filterIngredientInput.addEventListener('keyup', async (event) => {
+    filterListSection.innerHTML = ''
+    console.log(filterListSection)
+
+    const newFilteredIngredientsToDisplay = filteredIngredientsToDisplay.filter(el => {
+        return  el.toLowerCase().includes(event.target.value)
+    })
+    newFilteredIngredientsToDisplay.forEach(el => {
+        const filterListDOM = createFilterDomElement(el)
+        filterListSection.appendChild(filterListDOM);
+    })
 })
 
 async function getRecipes() {
@@ -24,14 +41,45 @@ async function getRecipes() {
     })
 }
 
-function displayData(recipes) {
+function displayData(recipes, inputValue) {
+    // Get recipe section and add class
     const recipeSection = document.getElementById("recipes");
     recipeSection.classList.add('recipes')
-    recipes.forEach((recipe) => {
+
+    // Get filtered Recipes
+    const filteredRecipes = getFilteredRecipes(recipes, inputValue)
+
+    // Create Recipe card
+    filteredRecipes.forEach((recipe) => {
         const recipeModel = recipeFactory(recipe);
         const recipeCardDOM = recipeModel.getRecipeCardDOM();
         recipeSection.appendChild(recipeCardDOM);
     });
+
+    // Set tags of filtered recipes
+    setFilteredListElements(filteredRecipes)
+}
+
+function getFilteredRecipes(recipes, inputValue) {
+    return recipes.filter(recipe => {
+        const result = isRecipeIncludingInputValueInNameOrDescription(recipe, inputValue)
+        if (result) return recipe
+    })
+}
+
+function setFilteredListElements(recipes) {
+    recipes.forEach(recipe => {
+        recipe.ingredients.forEach(ingredient => {
+            const ingredientName = ingredient.ingredient
+            displayFilterListElement(ingredientName)
+        })
+    })
+}
+
+function displayFilterListElement(data) {
+    const filterListDOM = createFilterDomElement(data)
+    filterListSection.appendChild(filterListDOM);
+    filteredIngredientsToDisplay.push(filterListDOM.innerText)
 }
 
 function displayTag(data) {
@@ -42,15 +90,15 @@ function displayTag(data) {
     tagSection.appendChild(tagDOM);
 }
 
-function displayFilterList(data) {
-    const filterListSection = document.getElementById("filter-elements");
-    filterListSection.classList.add('display-grid')
-    // const filterListModel = filterListFactory(data)
+function createFilterDomElement(data) {
     const filterListModel = recipeFactory(data)
     const filterListDOM = filterListModel.getFilterListDOM();
     filterListDOM.addEventListener('click', () => {
         displayTag(filterListDOM.innerText)
     })
-    filterListSection.appendChild(filterListDOM);
+    return filterListDOM
 }
 
+function isRecipeIncludingInputValueInNameOrDescription(recipe, inputValue) {
+   return recipe.name.toLowerCase().includes(inputValue) || recipe.description.toLowerCase().includes(inputValue + '')
+}
